dfd15b81ca4269055146c1b5db0c0cd9b8afc9ad
Arnaud Gaudard <gaudard@protonmail.ch>
2020-03-28 18:53:31 +0100

    corrections

--- ji_plan_back/plan/urls.py
+++ ji_plan_back/plan/urls.py
@@ -4,9 +4,9 @@ import plan.views as views
 
 app_name = 'plan'
 urlpatterns = [
-    path('at<int:pk>', views.AtView.as_view(), name='at'),
+    path('at/<int:pk>', views.AtView.as_view(), name='at'),
     path('create', views.CreateView.as_view(), name='create'),
-    path('at-and-inside<int:pk>', views.AtAndInsideView.as_view(), name='at_and_inside'),
+    path('at-and-inside/<int:pk>', views.AtAndInsideView.as_view(), name='at_and_inside'),
 ]
 
 # coming soon : search by attribute value
